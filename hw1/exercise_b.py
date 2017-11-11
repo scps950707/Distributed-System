16130ecc25f5b934aad99cb5de8c285a3d51e2e1
scps950707 <scps950707@gmail.com>
2017-11-11 20:57:38 +0800

    HW1 exercise_b add enum

--- hw1/exercise_b.py
+++ hw1/exercise_b.py
@@ -3,9 +3,18 @@
 # Author:         scps950707
 # Email:          scps950707@gmail.com
 # Created:        2017-10-26 15:13
-# Last Modified:  2017-10-28 17:17
+# Last Modified:  2017-11-11 20:57
 # Filename:       exercise_b.py
 # ==========================================================================
+from enum import Enum
+
+
+class CHO(Enum):
+    nDecided = -1
+    njoin = 0
+    join = 1
+
+
 nPoints = int(raw_input())
 Graph = []
 weight = [i for i in map(int, raw_input().split())]
@@ -14,14 +23,13 @@ for i in range(0, nPoints):
                   'weight': weight[i],
                   'edge': map(int, raw_input().split())})
 
-prev_round = [-1] * nPoints
-curr_round = [-1] * nPoints
-# -1:not decided, 0 not join MWIS, 1 join MWIS
+prev_round = [CHO.nDecided] * nPoints
+curr_round = [CHO.nDecided] * nPoints
 
 while True:
     for i in range(0, nPoints):
         # already decided
-        if prev_round[i] == 1 or prev_round[i] == 0:
+        if prev_round[i] == CHO.join or prev_round[i] == CHO.njoin:
             continue
 
         neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
@@ -29,12 +37,12 @@ while True:
         neiPrevState = [prev_round[n] for n in neighbors]
 
         # All neighbors not join MWIS
-        if len(neiPrevState) == neiPrevState.count(0):
-            curr_round[i] = 1
+        if len(neiPrevState) == neiPrevState.count(CHO.njoin):
+            curr_round[i] = CHO.join
             continue
         # One of neighbors join MWIS so not join MWIS
-        if 1 in neiPrevState:
-            curr_round[i] = 0
+        if CHO.join in neiPrevState:
+            curr_round[i] = CHO.njoin
             continue
         # Compare with neighbors to decide join or not
         isMax = True
@@ -44,7 +52,7 @@ while True:
             if tn > ti or (tn == ti and i > n):
                 isMax = False
         if isMax:
-            curr_round[i] = 1
+            curr_round[i] = CHO.join
     # Compare curr and prev and also copy curr result to prev
     end = True
     for i in range(0, nPoints):
@@ -53,4 +61,4 @@ while True:
             prev_round[i] = curr_round[i]
     if end:
         break
-print([i for i, v in enumerate(curr_round) if v == 1])
+print([i for i, v in enumerate(curr_round) if v == CHO.join])
