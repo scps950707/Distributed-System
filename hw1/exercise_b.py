81ee9d35bf78295d409884e75529041883e6c83b
scps950707 <scps950707@gmail.com>
2017-11-12 02:19:49 +0800

    Modify HW1 exercise_b.py
    1. Change all node's initialization from not decided to out of MWIS.
    2. Iterate each node to compare priority with neighbors before running
    simulate rounds.
    3. Termination check moved from end of round to start of round.
    4. If priority is not bigger than all neighbors will not join MWIS.

--- hw1/exercise_b.py
+++ hw1/exercise_b.py
@@ -3,14 +3,13 @@
 # Author:         scps950707
 # Email:          scps950707@gmail.com
 # Created:        2017-10-26 15:13
-# Last Modified:  2017-11-11 20:57
+# Last Modified:  2017-11-12 02:19
 # Filename:       exercise_b.py
 # ==========================================================================
 from enum import Enum
 
 
 class CHO(Enum):
-    nDecided = -1
     njoin = 0
     join = 1
 
@@ -23,36 +22,27 @@ for i in range(0, nPoints):
                   'weight': weight[i],
                   'edge': map(int, raw_input().split())})
 
-prev_round = [CHO.nDecided] * nPoints
-curr_round = [CHO.nDecided] * nPoints
+prev_round = [CHO.njoin] * nPoints
+curr_round = [CHO.njoin] * nPoints
+
+
+# init
+for i in range(0, nPoints):
+    neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
+                 if val == 1]
+    neiPrevState = [prev_round[n] for n in neighbors]
+    # Compare with neighbors to decide join or not
+    joinMWIS = True
+    ti = float(Graph[i]['weight'])/float(Graph[i]['edge'].count(1)+1)
+    for n in neighbors:
+        tn = float(Graph[n]['weight'])/float(Graph[n]['edge'].count(1)+1)
+        if tn > ti or (tn == ti and i > n):
+            joinMWIS = False
+            break
+    curr_round[i] = CHO.join if joinMWIS else CHO.njoin
+
 
 while True:
-    for i in range(0, nPoints):
-        # already decided
-        if prev_round[i] == CHO.join or prev_round[i] == CHO.njoin:
-            continue
-
-        neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
-                     if val == 1]
-        neiPrevState = [prev_round[n] for n in neighbors]
-
-        # All neighbors not join MWIS
-        if len(neiPrevState) == neiPrevState.count(CHO.njoin):
-            curr_round[i] = CHO.join
-            continue
-        # One of neighbors join MWIS so not join MWIS
-        if CHO.join in neiPrevState:
-            curr_round[i] = CHO.njoin
-            continue
-        # Compare with neighbors to decide join or not
-        isMax = True
-        ti = float(Graph[i]['weight'])/float(Graph[i]['edge'].count(1)+1)
-        for n in neighbors:
-            tn = float(Graph[n]['weight'])/float(Graph[n]['edge'].count(1)+1)
-            if tn > ti or (tn == ti and i > n):
-                isMax = False
-        if isMax:
-            curr_round[i] = CHO.join
     # Compare curr and prev and also copy curr result to prev
     end = True
     for i in range(0, nPoints):
@@ -61,4 +51,22 @@ while True:
             prev_round[i] = curr_round[i]
     if end:
         break
+    for i in range(0, nPoints):
+        neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
+                     if val == 1]
+        neiPrevState = [prev_round[n] for n in neighbors]
+
+        # All neighbors not join MWIS
+        if neiPrevState.count(CHO.join) == 0:
+            curr_round[i] = CHO.join
+            continue
+        # Compare with neighbors to decide join or not
+        joinMWIS = True
+        ti = float(Graph[i]['weight'])/float(Graph[i]['edge'].count(1)+1)
+        for n in neighbors:
+            tn = float(Graph[n]['weight'])/float(Graph[n]['edge'].count(1)+1)
+            if tn > ti or (tn == ti and i > n):
+                joinMWIS = False
+                break
+        curr_round[i] = CHO.join if joinMWIS else CHO.njoin
 print([i for i, v in enumerate(curr_round) if v == CHO.join])
