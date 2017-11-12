02acf231e2359d4fd847d9ab94c91427617d85df
scps950707 <scps950707@gmail.com>
2017-11-12 16:21:18 +0800

    Update HW2b HW2c

--- hw2/HW2c.py
+++ hw2/HW2c.py
@@ -3,11 +3,11 @@
 # Author:         scps950707
 # Email:          scps950707@gmail.com
 # Created:        2017-11-12 02:05
-# Last Modified:  2017-11-12 02:30
+# Last Modified:  2017-11-12 16:21
 # Filename:       HW2c.py
 # ==========================================================================
 from enum import Enum
-import random
+import itertools
 
 
 class CHO(Enum):
@@ -15,64 +15,64 @@ class CHO(Enum):
     join = 1
 
 
-nPoints = int(raw_input())
-Graph = []
-weight = [i for i in map(int, raw_input().split())]
-for i in range(0, nPoints):
-    Graph.append({'id': i,
-                  'weight': weight[i],
-                  'edge': map(int, raw_input().split())})
-
-# prev_round = [CHO.njoin] * nPoints
-prev_round = [None] * nPoints
-for i in range(0, nPoints):
-    prev_round[i] = random.choice([CHO.join, CHO.njoin])
-curr_round = [CHO.njoin] * nPoints
-
-
-# init
-for i in range(0, nPoints):
-    neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
-                 if val == 1]
-    neiPrevState = [prev_round[n] for n in neighbors]
-    # Compare with neighbors to decide join or not
-    joinMWIS = True
+def comparePriority(i, neighbors):
     ti = float(Graph[i]['weight'])/float(Graph[i]['edge'].count(1)+1)
     for n in neighbors:
         tn = float(Graph[n]['weight'])/float(Graph[n]['edge'].count(1)+1)
         if tn > ti and prev_round[n] == CHO.join:
-            joinMWIS = False
-            break
-    curr_round[i] = CHO.join if joinMWIS else CHO.njoin
-# print("random input:")
-# print([i.value for i in curr_round])
+            return CHO.njoin
+    return CHO.join
 
 
-while True:
-    # Compare curr and prev and also copy curr result to prev
-    end = True
-    for i in range(0, nPoints):
-        if prev_round[i] != curr_round[i]:
-            end = False
-            prev_round[i] = curr_round[i]
-    if end:
-        break
+def MWIS(prev_round, curr_round):
+    # init
     for i in range(0, nPoints):
         neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
                      if val == 1]
-        neiPrevState = [prev_round[n] for n in neighbors]
-
-        # All neighbors not join MWIS
-        if neiPrevState.count(CHO.join) == 0:
-            curr_round[i] = CHO.join
-            continue
         # Compare with neighbors to decide join or not
-        joinMWIS = True
-        ti = float(Graph[i]['weight'])/float(Graph[i]['edge'].count(1)+1)
-        for n in neighbors:
-            tn = float(Graph[n]['weight'])/float(Graph[n]['edge'].count(1)+1)
-            if tn > ti and prev_round[n] == CHO.join:
-                joinMWIS = False
-                break
-        curr_round[i] = CHO.join if joinMWIS else CHO.njoin
-print([i for i, v in enumerate(curr_round) if v == CHO.join])
+        curr_round[i] = comparePriority(i, neighbors)
+
+    loops = 1
+    notStop = False
+    while True:
+        if loops > 20:
+            print("not stop")
+            notStop = True
+            break
+        loops += 1
+        # Compare curr and prev and also copy curr result to prev
+        end = True
+        for i in range(0, nPoints):
+            if prev_round[i] != curr_round[i]:
+                end = False
+                prev_round[i] = curr_round[i]
+        if end:
+            break
+
+        for i in range(0, nPoints):
+            neighbors = [idx for idx, val in enumerate(Graph[i]['edge'])
+                         if val == 1]
+            neiPrevState = [prev_round[n] for n in neighbors]
+
+            # All neighbors not join MWIS
+            if neiPrevState.count(CHO.join) == 0:
+                curr_round[i] = CHO.join
+                continue
+            # Compare with neighbors to decide join or not
+            curr_round[i] = comparePriority(i, neighbors)
+    if not notStop:
+        print([i for i, v in enumerate(curr_round) if v == CHO.join])
+
+
+if __name__ == "__main__":
+    nPoints = int(raw_input())
+    Graph = []
+    weight = [i for i in map(int, raw_input().split())]
+    for i in range(0, nPoints):
+        Graph.append({'id': i,
+                      'weight': weight[i],
+                      'edge': map(int, raw_input().split())})
+    for seq in itertools.product([CHO.njoin, CHO.join], repeat=nPoints):
+        prev_round = list(seq)
+        curr_round = [CHO.njoin] * nPoints
+        MWIS(prev_round, curr_round)
